1fbb440d224fcb1ea42c42bc806b707661ec083e
Giovanni <pignoni.g@gmail.com>
2021-03-02 10:22:23 +0100

    foundation init

--- functions.php
+++ functions.php
@@ -140,13 +140,28 @@ add_action( 'widgets_init', 'francescobertele_widgets_init' );
  * Enqueue scripts and styles.
  */
 function francescobertele_scripts() {
+
+
+	wp_enqueue_style( 'francescobertele-style-foundation', get_template_directory_uri() . '/css/foundation.css' );
+
 	wp_enqueue_style( 'francescobertele-style', get_stylesheet_uri(), array(), _S_VERSION );
+
+	wp_enqueue_style( 'francescobertele-style-app', get_template_directory_uri() . '/css/app.css' );
+
 	wp_style_add_data( 'francescobertele-style', 'rtl', 'replace' );
+
+	wp_enqueue_script( 'francescobertele-foundation', get_template_directory_uri() . '/js/foundation.js', array(), _S_VERSION, true );
+
+	wp_enqueue_script( 'francescobertele-vendor', get_template_directory_uri() . '/js/vendor.js', array(), _S_VERSION, true );
 
 	wp_enqueue_script( 'francescobertele-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
+	wp_enqueue_script( 'francescobertele-app', get_template_directory_uri() . '/js/app.js', array(), _S_VERSION, true );
+
+
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+		/**wp_enqueue_script( 'comment-reply' ); */
 	}
 }
 add_action( 'wp_enqueue_scripts', 'francescobertele_scripts' );
