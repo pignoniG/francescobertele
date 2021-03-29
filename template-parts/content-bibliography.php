5d19b505d5691c5b15921ca5ba32e22ed94c879f
Giovanni <giovanpi@stud.ntnu.no>
2021-03-29 11:32:07 +0200

    bilbio init

---
+++ template-parts/content-bibliography.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package francescobertele
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<div class="grid-x">
+		BIBLIOGRA
+    
+</div>
+
+
+
+
+
+
+	
+	
+
+	
+</article><!-- #post-<?php the_ID(); ?> -->
