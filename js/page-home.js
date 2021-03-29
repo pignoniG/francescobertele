c08aefb092b2b861c0c30fd70a7e32a2c824eca4
Giovanni <giovanpi@stud.ntnu.no>
2021-03-29 11:42:27 +0200

    home

--- js/page-home.js
+++ js/page-home.js
@@ -1,8 +1,8 @@
 $(document).ready(function() 
     { 
 
-		setTimeout(function(){$(".logo_cover").fadeOut( "400");}, 1000);
-		setTimeout(function(){$("div.menu-buttons").addClass( "visible");}, 1000);
+		setTimeout(function(){$(".logo_cover").fadeOut( "1500");}, 6000);
+		setTimeout(function(){$("div.menu-buttons").addClass( "visible");}, 5000);
 		
 
     } 
