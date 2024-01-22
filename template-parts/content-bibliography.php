1f48c7f705957ed5d607c110a0283d68e245740a
Giovanni <pignoni.g@gmail.com>
2024-01-22 09:08:07 +0100

    Update content-bibliography.php
    cleanup bib

--- template-parts/content-bibliography.php
+++ template-parts/content-bibliography.php
@@ -10,97 +10,70 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
 	<div class="grid-x">
 		<div class="cell content_bib_body">
-
 			<div class="text-description-overflow-bibliography">
+		    		<h1 class="_TitleOU" style="border-bottom: 1px solid black;"> <?php the_title(); ?> </h1> 
+		    		<?php if( get_field('project_code') ):?><span class="_CatOU"><?php the_field('project_code'); ?> </span> <?php endif;?>
+		    		<?php if( get_field('project_isbn') ):?><span class="_CatOU">ISBN: <?php the_field('project_isbn'); ?> </span> <?php endif;?>
+		    		<?php
 
-		    <h1 class="_TitleOU" style="border-bottom: 1px solid black;"> <?php the_title(); ?> </h1> 
-		    <?php if( get_field('project_code') ):?><span class="_CatOU"><?php the_field('project_code'); ?> </span> <?php endif;?>
+				// Check rows exists.
+				if( have_rows('project_related_list') ):
 
-		    <?php if( get_field('project_isbn') ):?><span class="_CatOU">ISBN: <?php the_field('project_isbn'); ?> </span> <?php endif;?>
-
-
-
-		    <?php
-
-			// Check rows exists.
-			if( have_rows('project_related_list') ):
-
-				?> <p class="_BodyText" style="margin-bottom: 10px;"><?php echo  wpm_translate_string( "[:en]RELATED WORK[:it]LAVORI CORRELATI[:]", $language = '' ); ?>:<br>
+					?> <p class="_BodyText" style="margin-bottom: 10px;"><?php echo  wpm_translate_string( "[:en]RELATED WORK[:it]LAVORI CORRELATI[:]", $language = '' ); ?>:<br>
 					<?php
 
 			
-			    // Loop through rows.
-			    while( have_rows('project_related_list') ) : the_row();
+			    		// Loop through rows.
+			    		while( have_rows('project_related_list') ) : the_row();
 			
-			        // Load sub field value.
-			        $featured_post = get_sub_field('project_related');
-			        $permalink = get_permalink( $featured_post->ID );
-        			$title = get_the_title( $featured_post->ID );
+			       			// Load sub field value.
+			        		$featured_post = get_sub_field('project_related');
+			        		$permalink = get_permalink( $featured_post->ID );
+        					$title = get_the_title( $featured_post->ID );
+        					?><a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a>
+			   		<?php endwhile;?>
 
-        			?><a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a>
-
-			
-			    
-			   <?php endwhile;?>
-			
-		
-			</p><?php endif;?>
+					</p>
+				<?php endif;?>
 
 
-		    <?php if( get_field('project_exhibition') ):?> <p class="_BodyText" style="margin-bottom: 0px;"><?php echo  wpm_translate_string( "[:en]EXHIBITION[:it]ESPOSIZIONE[:]", $language = '' ); ?>:</p><?php the_field('project_exhibition'); ?><?php endif;?>
-
-
-		    <?php if( get_field('project_publisher') ):?> <p class="_BodyText" style="margin-bottom: 0px;"><?php echo  wpm_translate_string( "[:en]PUBLISHER[:it]EDITORE[:]", $language = '' ); ?>:</p><?php the_field('project_publisher'); ?><?php endif;?>
-
-
-
-		    <?php if( get_field('project_links') || get_field('project_files')  ):?> <p class="_BodyText" ><?php echo  wpm_translate_string( "[:en]SOURCES AND REFERENCE DOCUMENTS[:it]FONTI E DOCUMENTI DI RIFERIMENTO[:]", $language = '' ); ?>:
+		    		<?php if( get_field('project_exhibition') ):?> <p class="_BodyText" style="margin-bottom: 0px;"><?php echo  wpm_translate_string( "[:en]EXHIBITION[:it]ESPOSIZIONE[:]", $language = '' ); ?>:</p><?php the_field('project_exhibition'); ?><?php endif;?>
+		    		<?php if( get_field('project_publisher') ):?> <p class="_BodyText" style="margin-bottom: 0px;"><?php echo  wpm_translate_string( "[:en]PUBLISHER[:it]EDITORE[:]", $language = '' ); ?>:</p><?php the_field('project_publisher'); ?><?php endif;?>
+		<?php if( get_field('project_links') || get_field('project_files')  ):?> <p class="_BodyText" ><?php echo  wpm_translate_string( "[:en]SOURCES AND REFERENCE DOCUMENTS[:it]FONTI E DOCUMENTI DI RIFERIMENTO[:]", $language = '' ); ?>:
 		    	<br>
 
 
-		    	<?php if( have_rows('project_links') ):
-
+		    		<?php if( have_rows('project_links') ):
 					?> <br>../<?php echo  wpm_translate_string( "[:en]Links[:it]Risorse[:]", $language = '' ); ?>: <?php
-		    		// Loop through rows.
-		    		while( have_rows('project_links') ) : the_row();
+		    			// Loop through rows.
+		    			while( have_rows('project_links') ) : the_row();
+						?><a target="_blank" class="projects-link" href="<?php echo get_sub_field('project_link')['url']; ?>"><?php echo get_sub_field ('project_link')['title']; ?></a> <?php
 		
-		    		?><a target="_blank" class="projects-link" href="<?php echo get_sub_field('project_link')['url']; ?>"><?php echo get_sub_field		('project_link')['title']; ?></a> <?php
-		
-		    		endwhile;
-					?>  <?php 
-				endif ?> 
-
-
+		    			endwhile;
+					?>  
+				<?php endif ?> 
 				<?php if( have_rows('project_files') ):
-
 					?> <br>../<?php echo  wpm_translate_string( "[:en]Files[:it]Documenti[:]", $language = '' ); ?>: <?php
-		    		// Loop through rows.
-		    		while( have_rows('project_files') ) : the_row();
-		
-		    		?><a target="_blank" class="projects-link" href="<?php echo get_sub_field('project_pdf')['url']; ?>"><?php echo get_sub_field		('project_pdf')['filename']; ?></a> <?php
-		
-		    		endwhile;
-					?>  <?php 
-				endif ?> 
+		    			// Loop through rows.
+		    			while( have_rows('project_files') ) : the_row();
+		    				?><a target="_blank" class="projects-link" href="<?php echo get_sub_field('project_pdf')['url']; ?>"><?php echo get_sub_field ('project_pdf')['filename']; ?></a> <?php
+		    			endwhile;?>  
+				<?php endif ?> 
 
 		    </p><?php endif;?>
 		  
 		</div>
-
  		<h1 class="_TitleOU" style="border-bottom: 1px solid black;"> </h1> 
 		</div>
 
 
 		<div class="cell content_bib_image">
 			<?php 
-						$biblio_current_image = "";
+			$biblio_current_image = "";
 			   			     
-			   			     if( has_post_thumbnail()){
-									$biblio_current_image = fly_get_attachment_image_src( get_post_thumbnail_id(),'thumbnail-medium-for-interface' )['src'];	
-								}?>
+			if( has_post_thumbnail()){ $biblio_current_image = fly_get_attachment_image_src( get_post_thumbnail_id(),'thumbnail-medium-for-interface' )['src'];}?>
 
 			<img src="<?php echo $biblio_current_image; ?>">
 		</div>
