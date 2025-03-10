1487138a7acc196557800723f671b63ff966684f
giovanni <pignoni.g@gmail.com>
2025-03-10 11:19:48 +0100

    apertura immagini dentro ouvre e onair

--- js/single-onair.js
+++ js/single-onair.js
@@ -2,6 +2,55 @@ $( document ).ready(
     function(){ 
 
 
+onDomChange(function(){ 
+   
+        $('.wp-block-image img').not(".img-enlargeable").addClass('img-enlargeable').click(function() {
+var src = $(this).attr('src');
+
+  if ($(this).attr("srcset")) {
+      src =$(this).attr("srcset").split(",").reduce(
+        (acc, item) => {
+          let [url, width] = item.trim().split(" ");
+          width = parseInt(width);
+          if (width > acc.width) return { width, url };
+          return acc;
+        },
+        { width: 0, url: "" }
+      ).url;
+
+  }
+
+  
+  var modal;
+
+  function removeModal() {
+    modal.remove();
+    $('body').off('keyup.modal-close');
+  }
+  modal = $('<div>').css({
+    background: 'RGBA(0,0,0,.8) url(' + src + ') no-repeat center',
+    backgroundSize: 'contain',
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    zIndex: '10000',
+    top: '0',
+    left: '0',
+    cursor: 'zoom-out'
+  }).click(function() {
+    removeModal();
+  }).appendTo('body');
+  //handling ESC
+  $('body').on('keyup.modal-close', function(e) {
+    if (e.key === 'Escape') {
+      removeModal();
+    }
+  });
+});
+
+});
+
+
 
   $("#onair-more").click(function(){
         $("#onair-description-container").toggleClass("blocked");
