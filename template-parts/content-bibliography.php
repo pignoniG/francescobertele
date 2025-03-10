4eb07c68af0e29685b7ab41b8d7e90fd02a27aca
giovanni <pignoni.g@gmail.com>
2025-03-10 12:47:58 +0100

    griglia bilblio

--- template-parts/content-bibliography.php
+++ template-parts/content-bibliography.php
@@ -73,12 +73,20 @@
 
 
 		<div class="cell content_bib_image">
-			<?php 
-			$biblio_current_image = "";
-			   			     
-			if( has_post_thumbnail()){ $biblio_current_image = fly_get_attachment_image_src( get_post_thumbnail_id(),'thumbnail-medium-for-interface' )['src'];}?>
 
-			<img src="<?php echo $biblio_current_image; ?>">
+			
+			<?php 
+			$images = get_field('biblio_gallery');
+			$size = 'full'; // (thumbnail, medium, large, full or custom size)
+			if( $images ): ?>
+			        <?php foreach( $images as $image_id ): ?>
+			          
+			                <?php echo wp_get_attachment_image( $image_id, $size ); ?>
+			           
+			        <?php endforeach; ?>
+			   
+			<?php endif; ?>
+
 		</div>
 
 
