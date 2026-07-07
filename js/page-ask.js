7687c67a85bae2c49b63eecdf9b73f41d08850c7
Giovanni <pignoni.g@gmail.com>
2026-07-07 22:44:24 +0200

    Fix class check method for lastElement

--- js/page-ask.js
+++ js/page-ask.js
@@ -136,8 +136,8 @@ $(document).ready(function()
       
 
       if (topElement != lastElement) {
-		  console.log(lastElement.classList);
-		  if (!lastElement.classList.contains("hiddetoallbutme")){
+
+		  if (!lastElement.hasClass("hiddetoallbutme")){
         		var bottone= $('[data-targhet="#'+topElement.attr("id")+'"]')
         
 
