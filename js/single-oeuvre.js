ca8df31c36011dd6ef701cc3f9ad109bea69de15
Giovanni <pignoni.g@gmail.com>
2026-07-20 09:53:18 +0200

    Refactor image click handler in single-oeuvre.js

--- js/single-oeuvre.js
+++ js/single-oeuvre.js
@@ -1,9 +1,9 @@
 
 $( document ).ready(
     function(){ 
-onDomChange(function(){ 
+
    
-        $('.wp-block-image img').not(".img-enlargeable").addClass('img-enlargeable').click(function() {
+        $('.wp-block-image img').click(function() {
 var src = $(this).attr('src');
 
   if ($(this).attr("srcset")) {
@@ -47,7 +47,7 @@ var src = $(this).attr('src');
   });
 });
 
-});
+
 
 
 
